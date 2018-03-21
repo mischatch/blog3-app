49b7e4046c3982941b674896df456ff838d2e725
Mikhail Cheburakhtin <mischa.tch@gmail.com>
2018-03-21 23:53:11 +0300

    fixing signup actions

--- src/signUp/signUpContainer.js
+++ src/signUp/signUpContainer.js
@@ -1,6 +1,11 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
-const mapStateToProps = state => {
+// const mapStateToProps = state => {
+//
+// };
 
-};
+
+const mapDispatchToProps = dispatch => {
+  signup: authUser => 
+}
